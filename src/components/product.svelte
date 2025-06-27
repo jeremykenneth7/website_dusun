651c8d9bfaa4602e1109c6a9b8f9f9ece8376a21
jeremykenneth7 <jeremykenneth7@gmail.com>
2025-06-28 00:42:54 +0700

    Tambah halaman product di website

--- src/components/product.svelte
+++ src/components/product.svelte
@@ -1,7 +1,29 @@
 <script>
 	import Sectionwrapper from './sectionwrapper.svelte';
+	let productFeatures = [];
 </script>
 
-<Sectionwrapper id="project">
-	
+<Sectionwrapper id="product">
+	<div
+		class="flex flex-1 flex-col items-center justify-center gap-10 pb-10 sm:gap-14 md:gap-24 md:pb-14"
+	>
+		<div class="flex flex-col gap-2">
+			<p class="text-center text-base opacity-60 sm:text-lg md:text-xl">
+				Test <span class="text-indigo-400">Testtttt</span>
+			</p>
+			<h3
+				class="mx-auto w-full max-w-[1000px] text-center text-4xl font-semibold sm:text-5xl md:text-6xl"
+			>
+				<span class="text-indigo-400">Test</span> Product
+			</h3>
+		</div>
+		<div class="relative grid grid-cols-1 gap-14 text-base md:grid-cols-2 md:gap-14 lg:gap-20">
+			{#each productFeatures as feature}
+				<div class="flex flex-col gap-4">
+					<h4 class="text-lg font-semibold">{feature.title}</h4>
+					<p class="text-gray-600">{feature.description}</p>
+				</div>
+			{/each}
+		</div>
+	</div>
 </Sectionwrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Hero from '../components/hero.svelte';
+	import Product from '../components/product.svelte';
 </script>
 
 <main class="flex flex-col">
 	<Hero />
+	<Product />
 </main>
