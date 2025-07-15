0bf1cbc72b505cfdb6a1b1ab3521de604cf80619
jeremykenneth7 <jeremykenneth7@gmail.com>
2025-07-16 00:14:16 +0700

    Buat section untuk sambutan

--- src/components/sambutan.svelte
+++ src/components/sambutan.svelte
@@ -5,23 +5,23 @@
 	const jabatan = 'Dosen Pembimbing Lapangan';
 	const sambutan = `
     <b>Assalamu'alaikum Warahmatullahi Wabarakatuh</b><br>
-        Selamat datang di website Dusun Druju Tegal. 
+        Selamat datang di Website Dusun Druju Tegal. 
         Saya sangat mengapresiasi semangat dan kerja keras seluruh tim KKN dalam membangun dan mengembangkan potensi desa ini.
         Semoga website ini dapat menjadi sarana informasi dan komunikasi yang bermanfaat bagi masyarakat dan pihak terkait.
         Teruslah berkarya dan berinovasi demi kemajuan bersama.
         Salam hangat dan sukses selalu untuk kita semua.
         <br><br>
-        Hormat saya,<br>
-        Isbandi Sutrisno, S.Sos, M.Si.
+        <b>Hormat saya,<br>
+        Isbandi Sutrisno, S.Sos, M.Si.</b>
     `;
 </script>
 
 <SectionWrapper id="sambutan" class="pb-0">
-	<div class="mx-23 mt-10 max-w-xl p-6">
+	<div class="mx-18 mt-5 max-w-xl p-6">
 		<h1 class="mb-2 text-4xl font-bold">{intro}</h1>
 		<h2 class="mb-2 text-2xl font-bold">{nama}</h2>
 		<h3 class="mb-2 text-lg text-gray-600">{jabatan}</h3>
-		<div class="h-64 overflow-y-auto rounded bg-white" style="max-height: 16rem;">
+		<div class="h-64 overflow-y-auto rounded bg-white" style="max-height: 10rem;">
 			<div class="text-gray-800">{@html sambutan}</div>
 		</div>
 	</div>
