65ae46fbeaa340115b8e0d256a04429c91da5279
jeremykenneth7 <jeremykenneth7@gmail.com>
2025-07-16 23:23:32 +0700

    Fix mb di sambutan

--- src/components/sambutan.svelte
+++ src/components/sambutan.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Sectionwrapper from './sectionwrapper.svelte';
-	const intro = 'Sambutan Kepala Dusun Druju Tegal';
+	const intro = 'SAMBUTAN KEPALA DUSUN DRUJU TEGAL';
 	const nama = 'Mukhlis Arifin';
 	const jabatan = 'Kepala Dusun Druju Tegal';
 	const sambutan = `
@@ -22,9 +22,9 @@
 			class="h-32 w-32 rounded object-contain sm:h-1/3 sm:w-1/3"
 		/>
 		<div>
-			<h1 class="mb-2 text-2xl font-bold sm:text-4xl">{intro}</h1>
-			<h2 class="mb-1 text-lg font-bold sm:text-2xl">{nama}</h2>
-			<h3 class="mb-2 text-base text-gray-600 sm:text-lg">{jabatan}</h3>
+			<h1 class="text-2xl font-bold sm:text-4xl">{intro}</h1>
+			<h2 class="text-lg font-bold sm:text-2xl">{nama}</h2>
+			<h3 class="mb-3 text-base text-gray-600 sm:text-lg">{jabatan}</h3>
 			<div
 				class="h-40 overflow-y-scroll rounded bg-white sm:h-64"
 				style="max-height: 10rem; scrollbar-width: auto; scrollbar-color: #a0aec0 #edf2f7;"
