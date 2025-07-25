c23805b30736f43d58d984d325b643c6101fcbe7
jeremykenneth7 <jeremykenneth7@gmail.com>
2025-07-25 12:32:57 +0700

    Hapus config data

--- src/components/floating_kunjungan.svelte
+++ src/components/floating_kunjungan.svelte
@@ -4,14 +4,14 @@
 	import { initializeApp, getApps } from 'firebase/app';
 
 	const firebaseConfig = {
-		apiKey: 'AIzaSyAVI0FRUDLLTxkBRYEuAxFKj9xpfkEm5Ck',
-		authDomain: 'websitedusundrujutegal.firebaseapp.com',
-		projectId: 'websitedusundrujutegal',
-		storageBucket: 'websitedusundrujutegal.appspot.com',
-		messagingSenderId: '178102873490',
-		appId: '1:178102873490:web:284c1b78586acdbed0654b',
-		measurementId: 'G-X1JMSD4XT4',
-		databaseURL: 'https://websitedusundrujutegal-default-rtdb.asia-southeast1.firebasedatabase.app/'
+		apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+		authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+		projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+		storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+		appId: import.meta.env.VITE_FIREBASE_APP_ID,
+		measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+		databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 	};
 
 	let app;
