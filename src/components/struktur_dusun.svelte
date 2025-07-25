132e8d9e49fd91b0dc0ac32cb7de9064ee57cdac
jeremykenneth7 <jeremykenneth7@gmail.com>
2025-07-25 13:25:37 +0700

    Update label struktur organisasi dan stuktur pemuda pemudi

--- src/components/struktur_dusun.svelte
+++ src/components/struktur_dusun.svelte
@@ -5,7 +5,7 @@
 
 <Sectionwrapper id="struktur_dusun" class="pb-0">
 	<div class="mx-4 flex max-w-4xl flex-col items-start px-2 sm:mx-18 sm:px-6">
-		<h2 class="mt-2 text-left text-3xl font-bold">STRUKTUR DUSUN DRUJU TEGAL</h2>
+		<h2 class="mt-2 text-left text-3xl font-bold">STRUKTUR PERANGKAT DUSUN DRUJU TEGAL</h2>
 		<h3 class="text-left text-xl">
 			Struktur organisasi dan kepengurusan Dusun Druju Tegal yang berperan dalam pengelolaan dan
 			pembangunan wilayah.
--- src/components/struktur_pemuda.svelte
+++ src/components/struktur_pemuda.svelte
@@ -5,9 +5,9 @@
 
 <Sectionwrapper id="struktur_dusun" class="pb-0">
 	<div class="mx-4 flex max-w-4xl flex-col items-start px-2 sm:mx-18 sm:px-6">
-		<h2 class="mt-2 text-left text-3xl font-bold">STRUKTUR PEMUDA DUSUN DRUJU TEGAL</h2>
+		<h2 class="mt-2 text-left text-3xl font-bold">STRUKTUR ORGANISASI IKATAN PEMUDA-PEMUDI DUSUN DRUJU TEGAL</h2>
 		<h3 class="text-left text-xl">
-			Struktur organisasi pemuda Dusun Druju Tegal yang berperan dalam kegiatan sosial dan
+			Struktur organisasi ikatan pemuda-pemudi Dusun Druju Tegal yang berperan dalam kegiatan sosial dan
 			pembangunan komunitas.
 		</h3>
 		<br>
